68ad8969ceb76f9263830ed49187355b1b5bc657
guisimon20 <guigvsimon20@gmail.com>
2024-07-09 19:48:31 -0300

    Update main.py

--- store/main.py
+++ store/main.py
@@ -10,7 +10,7 @@ class App(FastAPI):
             *args,
             **kwargs,
             version="0.0.1",
-            title=settings.PROJECT_NAME,
+            title=settings.STORE_API,
             root_path=settings.ROOT_PATH
         )
 
